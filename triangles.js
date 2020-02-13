77b1ff6aedff7067c258eef9a5eb02094dbf1d58
DevRNelson <59750468+DevRNelson@users.noreply.github.com>
2020-02-13 18:13:17 +0800

    First Commit

---
+++ triangles.js
@@ -0,0 +1,9 @@
+<script>
+
+let triangles, side1, side2, side3, scalene, isosceles, equilateral
+
+triangles: prompt("Please enter the type of Triangle (Scalene, Isosceles, Equilateral)");
+
+
+</script>
+

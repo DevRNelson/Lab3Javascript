48a4a4ffecf6a1d9e41d163c7f0497bc3ce725f9
DevRNelson <59750468+DevRNelson@users.noreply.github.com>
2020-02-13 18:34:37 +0800

    Second Commit

--- triangles.js
+++ triangles.js
@@ -3,12 +3,10 @@ let triangles, side1, side2, side3, Scalene, Isosceles, Equilateral
 
 alert("This Program will find detect types of triangles through their sides")
 
-if (triangles === Scalene) {
- scalside1: prompt("Please enter the value of the first side")
- scalside2: prompt("Please enter the value of the second side")
- scalside3: prompt("Please enter the value of the third side")
+side1: prompt("Please enter the value of the first side")
+side2: prompt("Please enter the value of the second side")
+side3: prompt("Please enter the value of the third side")
  
-}
 
 </script>
 

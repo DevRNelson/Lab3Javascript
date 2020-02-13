5f745cd51ccccf3eecdf70b424b1728ccbf1b140
DevRNelson <59750468+DevRNelson@users.noreply.github.com>
2020-02-13 19:19:42 +0800

    Fourth Commit

--- triangles.js
+++ triangles.js
@@ -1,22 +1,23 @@
 <script>
-let triangles, side1, side2, side3, PreScalene, PreScalene2, Scalene,  PreEqui2,
- PreEqui, Isosceles, Equilateral 
+let triangles, side1, side2, side3, PreScalene, PreScalene2, Scalene,  PreEqui2, 
+EquiAnswer, PreEqui, Isosceles, Equilateral
 
-alert("This Program will find detect types of triangles through their sides")
+alert("This Program will find detect types of triangles through their sides");
 
-side1: prompt("Please enter the value of the first side")
-side2: prompt("Please enter the value of the second side")
-side3: prompt("Please enter the value of the third side")
+side1: prompt("Please enter the value of the first side");
+side2: prompt("Please enter the value of the second side");
+side3: prompt("Please enter the value of the third side");
  
  
-PreEqui: (side1 + side2)
-PreEqui2: (side3 * 2)
+PreEqui: (side1 + side2);
+PreEqui2: (side3 * 2);
  
 if  (PreEqui == PreEqui2) {
-
-	alert("Your Triangle is an Equilateral Triangle!")
-
+	EquiAnswer: prompt("What Type of triangle do you think it was? (Equilateral, Scalene, Isosceles)");
+    	if (EquiAnswer == "Equilateral", "equilateral") {       
+       alert("You are correct your Triangle is an Equilateral Triangle!");
+        }
+	
 }
 
 </script>
-

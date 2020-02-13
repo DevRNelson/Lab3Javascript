92006d4d41789a2d44f2679fe84db286ceaa2093
DevRNelson <59750468+DevRNelson@users.noreply.github.com>
2020-02-13 18:25:03 +0800

    Second Commit

--- triangles.js
+++ triangles.js
@@ -1,9 +1,11 @@
 <script>
+let triangles, scalside1, scalside2, scalside3, iside1, iside2, iside3, 
+Scalene, Isosceles, Equilateral 
 
-let triangles, side1, side2, side3, scalene, isosceles, equilateral
+triangles: prompt("Please enter the type of Triangle (Scalene, Isosceles, Equilateral)")
 
-triangles: prompt("Please enter the type of Triangle (Scalene, Isosceles, Equilateral)");
-
+if (triangles === Scalene) {
+ scalside1: prompt("Please enter the value of the first side")
+}
 
 </script>
-
